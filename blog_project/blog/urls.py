a452faccd6d1eee42967b38f1c69f7b933f74103
khabindra <pujakhabi78@gmail.com>
2023-10-30 17:36:36 +0545

    adding individual page in Blog

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import BlogListView
+from .views import BlogListView,DetailView 
 urlpatterns = [
     path('',BlogListView.as_view(),name='home'),
-    
+    path('post/<int:pk>',DetailView.as_view(),name='post_detail'),
+
+
 ]
--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from .models import Post
 # Create your views here
 class BlogListView(ListView):
     model = Post
     template_name = "home.html"
+
+class DetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
