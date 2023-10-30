1711e537ef096ee7830d37cde0a5cb4318ee44e8
khabindra <pujakhabi78@gmail.com>
2023-10-30 22:44:51 +0545

    i made a sign up page for the blog project

--- blog_project/blog_project/urls.py
+++ blog_project/blog_project/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('django.contrib.auth.urls')),
     path('',include('blog.urls')),
+    path('accounts/',include('accounts.urls')),
+
 ]
