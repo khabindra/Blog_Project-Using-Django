d1461f7b1416c2620c0e69dee37c963ac377a836
khabindra <pujakhabi78@gmail.com>
2023-10-30 19:56:30 +0545

    i wrote a code for deleting the Post in the Blog

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
-from django.views.generic.edit import CreateView,UpdateView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView 
+from django.urls import reverse_lazy
 from .models import Post
 # Create your views here
 class BlogListView(ListView):
@@ -20,3 +21,8 @@ class BlogUpdateView(UpdateView):
     model = Post
     template_name = "post_edit.html"
     fields = ['title','body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy('home')
