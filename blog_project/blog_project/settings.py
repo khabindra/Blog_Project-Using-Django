5b9870f1c45fd51a6d273f14433a4b83d751f445
khabindra <pujakhabi78@gmail.com>
2023-10-30 22:15:15 +0545

    i wrote a code for the login page

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
+LOGIN_REDIRECT_URL ='home'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
